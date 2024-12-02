0bc6d1d6e3e902ec983574c584b65ed3809f70bc
Renato Bessa <bessache@outlook.com>
2024-12-02 07:39:46 -0300

    commit aula caso de uso de registro

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
-import { hash } from 'bcryptjs'
+import { registerUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, replay: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,21 +10,15 @@ export async function register(request: FastifyRequest, replay: FastifyReply) {
   })
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-  if (userWithSameEmail) {
-    return replay.status(409).send()
-  }
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      password_hash,
-    },
-  })
+      password,
+    })
+  } catch (err) {
+    return replay.status(409).send()
+  }
+
   return replay.status(201).send()
 }
