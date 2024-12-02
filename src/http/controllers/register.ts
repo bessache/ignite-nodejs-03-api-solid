7f51181bbf9e759276b53256c219ce03abb30a53
Renato Bessa <bessache@outlook.com>
2024-12-02 07:20:46 -0300

    commit aula hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, replay: FastifyReply) {
   const registerBodySchema = z.object({
@@ -9,11 +10,21 @@ export async function register(request: FastifyRequest, replay: FastifyReply) {
     password: z.string().min(6),
   })
   const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+  if (userWithSameEmail) {
+    return replay.status(409).send()
+  }
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
   return replay.status(201).send()
