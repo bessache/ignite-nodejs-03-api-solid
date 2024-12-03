66f4fab1c80e6e71a613f4f6c20276bd8748efea
Renato Bessa <bessache@outlook.com>
2024-12-03 06:23:51 -0300

    Commit aulas interface do repositorio, lidando com erros e handlre erros global

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
 
 export async function register(request: FastifyRequest, replay: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,14 +13,18 @@ export async function register(request: FastifyRequest, replay: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+    await registerUseCase.execute({
       name,
       email,
       password,
     })
   } catch (err) {
-    return replay.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return replay.status(409).send({ message: err.message })
+    }
+    throw err
   }
-
   return replay.status(201).send()
 }
